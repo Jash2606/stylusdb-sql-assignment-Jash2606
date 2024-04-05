73f9dc2ee6264ebdbeb7d85e4c028e2c60dd9c7f
Jash Savaliya <jash.23bcs10163@sst.scaler.com>
2024-04-05 23:26:47 +0530

    Till Step 8

--- tests/step-02/index.test.js
+++ tests/step-02/index.test.js
@@ -1,27 +1,9 @@
 const readCSV = require('../../src/csvReader');
 
 test('Read CSV File', async () => {
-    const data = await readCSV('./sample.csv');
+    const data = await readCSV('./student.csv');
     expect(data.length).toBeGreaterThan(0);
     expect(data.length).toBe(3);
     expect(data[0].name).toBe('John');
     expect(data[0].age).toBe('30'); //ignore the string type here, we will fix this later
 });
-const parseQuery = require('./queryParser');
-const readCSV = require('./csvReader');
-
-async function executeSELECTQuery(query) {
-    const { fields, table } = parseQuery(query);
-    const data = await readCSV(`${table}.csv`);
-    
-    // Filter the fields based on the query
-    return data.map(row => {
-        const filteredRow = {};
-        fields.forEach(field => {
-            filteredRow[field] = row[field];
-        });
-        return filteredRow;
-    });
-}
-
-module.exports = executeSELECTQuery;
